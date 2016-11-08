69336d4be8a71bf7a7b3ec73f7cb0c2957d0331f
Chandler Cain <chandler@clcain.com>
2016-11-07 20:47:34 -0500

    Check for existing tokens.conf

--- src/install.sh
+++ src/install.sh
@@ -3,7 +3,7 @@
 if [ ! -e "$PWD/install.sh" ]; then echo "Installation script must be run from the src/ directory."; exit 1; fi
 pushd ../ >> /dev/null
 mkdir ~/.bash-2fa &> /dev/null
-cp .bash-2fa/tokens.conf ~/.bash-2fa
+if [ ! -e ~/.bash-2fa/tokens.conf ]; then echo "tokens.conf not found, installing template."; cp .bash-2fa/tokens.conf ~/.bash-2fa; fi
 sudo cp src/2fa.sh /usr/local/bin/2fa
 sudo chmod +x /usr/local/bin/2fa
 popd >> /dev/null
