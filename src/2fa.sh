4b9e455dc0618ba81b2753ed46f769921fe87b97
Chandler Cain <chandler@clcain.com>
2018-10-21 16:38:29 -0400

    Add 32 bit key support and update tokens.conf format

--- src/2fa.sh
+++ src/2fa.sh
@@ -2,10 +2,11 @@
 
 if [ -z "$1" ]; then echo "No token name parameter given."; exit 1; fi
 
-IFS="="
-while read -r name token
+IFS=","
+while read -r name token bits
 do
-    if [ "$1" = "$name" ]; then oathtool --totp "$token"; exit 0; fi
+    if [ "$1" = "$name" ] && [ "$bits" = "32" ]; then oathtool --totp -b "$token"; exit 0; fi
+    if [ "$1" = "$name" ] && [ "$bits" = "64" ]; then oathtool --totp "$token"; exit 0; fi
 done < ~/.bash-2fa/tokens.conf
 
 echo "No entry found for "$1"."
